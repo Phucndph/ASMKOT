d1fea4ab7a75dc35e263c24fd78a1b3779a2caea
Phucndph <phucndph42684@fpt.edu.vn>
2024-06-11 15:34:39 +0700

    main

---
+++ app/src/main/java/com/example/asskot/models/Category.kt
@@ -0,0 +1,7 @@
+package com.example.asskot.models
+
+data class Category(
+    val id: String,
+    val cateName: String,
+    val cateImg: String
+)
---
+++ app/src/main/java/com/example/asskot/models/Favorite.kt
@@ -0,0 +1,12 @@
+package com.example.asskot.models
+
+class Favorite(
+    val id: String,
+    val description: String,
+    val prName: String,
+    val prPrice: String,
+    val quantity: Int,
+    val rate: String,
+    val prImg: String,
+    val prId: String,
+)
